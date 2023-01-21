ff73a3cf9921580a3c45a215e13f8fc2986bcc8d
13. Xaymon Xayyabupha <xaymonmohn@gmail.com>
2023-01-21 16:11:42 +0700

    One

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '12jack@@',
-    database: 'vite',
+    database: 'saimon',
     port: 3308
 })
 
@@ -56,7 +56,7 @@ app.use(bodyParser.json())
 // });
 
 app.get('/', (req, res) => {
-    var sql = 'SELECT user_id, username FROM user'
+    var sql = 'SELECT id, username FROM user'
     // var sql = "INSERT INTO user (username, password) VALUES('anny', 1234)"
     connection.query(sql, (err, rows, fields) => {
         if (err) throw err;
@@ -72,10 +72,10 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('/get/:user_id', (req, res) => {
-    var sql = 'SELECT * FROM user WHERE user_id=?'
+app.get('/get/:id', (req, res) => {
+    var sql = 'SELECT * FROM user WHERE id=?'
     // var sql = "INSERT INTO user (username, password) VALUES('anny', 1234)"
-    connection.query(sql, [req.params.user_id], (err, rows, fields) => {
+    connection.query(sql, [req.params.id], (err, rows, fields) => {
         if (err) throw err;
 
         // var data = {
@@ -114,13 +114,13 @@ app.post('/create', (req, res) => {
 app.post('/login', (req, res) => {
     var { email, password } = req.body
     var params = [email, password]
-    var sql = "SELECT user_id FROM user WHERE email = ? AND password = ?"
+    var sql = "SELECT id FROM user WHERE email = ? AND password = ?"
     connection.query(sql, params, (err, rows, fields) => {
         if (err) {
             throw err
         } else {
             if (rows.length > 0) {
-                res.cookie("login", rows[0].user_id)
+                res.cookie("login", rows[0].id)
                 let data = {
                     status: 200,
                     message: 'Log In'
@@ -136,13 +136,13 @@ app.post('/login', (req, res) => {
 app.post('/isLoggedIn', (req, res) => {
     //     var { email, password } = req.body
     //     var params = [email, password]
-    //     var sql = "SELECT user_id FROM user WHERE email = ? AND password = ?"
+    //     var sql = "SELECT id FROM user WHERE email = ? AND password = ?"
     //     connection.query(sql, params, (err, rows, fields) => {
     //         if (err) {
     //             throw err
     //         }else {
     //             if (rows.length > 0) {
-    //                 res.cookie("login", rows[0].user_id)
+    //                 res.cookie("login", rows[0].id)
     //                 let data = {
     //                     status: 200,
     //                     message: 'Log In'
@@ -164,9 +164,9 @@ app.post('/isLoggedIn', (req, res) => {
 })
 
 app.put('/update', (req, res) => {
-    var { firstname, lastname, username, email, password, user_id } = req.body
-    var params = [firstname, lastname, username, email, password, user_id]
-    var sql = "UPDATE user SET firstname = ?, lastname = ?, username = ?, email = ?, password = ? WHERE user_id = ?"
+    var { firstname, lastname, username, email, password, id } = req.body
+    var params = [firstname, lastname, username, email, password, id]
+    var sql = "UPDATE user SET firstname = ?, lastname = ?, username = ?, email = ?, password = ? WHERE id = ?"
     connection.query(sql, params, (err, rows, fields) => {
         if (err) throw err;
 
@@ -182,9 +182,9 @@ app.put('/update', (req, res) => {
 })
 
 app.delete('/delete', (req, res) => {
-    // var {username, email, user_id} = req.body
-    // var params = [username, email, user_id]
-    var sql = "DELETE FROM user WHERE user_id = 12"
+    // var {username, email, id} = req.body
+    // var params = [username, email, id]
+    var sql = "DELETE FROM user WHERE id = 12"
     connection.query(sql, [], (err, rows, fields) => {
         if (err) throw err;
 
